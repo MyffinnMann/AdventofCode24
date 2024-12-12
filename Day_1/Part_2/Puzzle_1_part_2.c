e8874e63bde8f365cc83d549a94d49c3d8a1cb86
MyffinnMann <126001958+MyffinnMann@users.noreply.github.com>
2024-12-12 00:34:21 +0000

    Fixed files and soved part 2 from day 1.

--- Day_1/Part_2/Puzzle_1_part_2.c
+++ Day_1/Part_2/Puzzle_1_part_2.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
-}
+int compare(const void *a, const void *b);
+int countNrOcc(int arr[], int n, int target);
 
 int main() {
     FILE *file = fopen("input.txt", "r");  // Open the input file for reading
@@ -16,7 +15,7 @@ int main() {
     int left_list[1000];
     int right_list[1000];
     int size = 0;
-
+    int similarity = 0;
     // Read the file line by line Each line contains one number for both lists
     while (fscanf(file, "%d %d", &left_list[size], &right_list[size]) == 2) {
         size++;
@@ -24,18 +23,29 @@ int main() {
 
     fclose(file);  // Close the file after reading
 
-    // Step 1: Sort both lists
+    // Sort both lists
     qsort(left_list, size, sizeof(int), compare);
     qsort(right_list, size, sizeof(int), compare);
-
-    // Step 2: Calculate the total distance
-    int total_distance = 0;
-    for (int i = 0; i < size; i++) {
-        total_distance += abs(left_list[i] - right_list[i]);
+  
+    for (int i = 0; i < size; i++){
+        int target = left_list[i]; // This is what will be the single number in the left list
+        int NumberOfOccurences = countNrOcc(right_list, size, target); 
+        similarity += (target * NumberOfOccurences);
     }
-
-    // Output the total distance
-    printf("Total Distance: %d\n", total_distance);
-
+    printf("%d\n", similarity);
     return 0;
+}
+
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int countNrOcc(int arr[], int n, int target){
+    int res = 0;
+    for (int i = 0; i < n; i++){
+        if (arr[i] == target){
+            res++;
+        }
+    }
+    return res;
 }
